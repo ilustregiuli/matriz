91f2fdd7e21bf504b428c2cd1fa98f399f002ad2
Giuli Ilustre <ilustre.giuli@gmail.com>
2020-10-29 08:46:02 -0300

    Aplicando comentários nas minhas soluções e do Professor

--- ApplicantionII.java
+++ ApplicantionII.java
@@ -33,15 +33,43 @@ public class ApplicantionII {
 				
 				if(matriz[i][j] == x) {
 					System.out.println("Position: " + i +"," +j);
+					// vizinho da esquerda, minha solução:
 						if((j-1) >= 0) {
 							System.out.println("Left: " + matriz[i][j-1]);
 						}
+					/* "se a posição da coluna anterior (j-1) for maior ou igual a 0, é porque
+					 	ela existe..."
+					 	
+					 	Solução do professor:
+					 		if (j > 0) {
+								System.out.println("Left: " + mat[i][j-1]);
+							}	
+						"Se a posição da coluna for maior que Zero, então depois eu posso diminuir
+						-1 e mostrar o conteúdo dessa posição na tela. Caso for 0, essa posição 
+						tem vizinho."	
+					*/
+						
 						if((j+1) <= (matriz[i].length-1)) {
 							System.out.println("Right: " + matriz[i][j+1]); 
 						}
+						
 						if((i+1) <= (matriz.length-1)) {
 							System.out.println("Down: " + matriz[i+1][j]); 
 						}
+						/* "Se a linha de baixo (i+1) for menor ou igual ao último índice
+						 *  (nesse caso, .length -1, que retorna o número do último índice) então
+						 *  essa posição existe..."
+						 *  
+						 *  Solução do professor:
+						 *  
+						 *  if (i < mat.length-1) {
+								System.out.println("Down: " + mat[i+1][j]);
+							}
+							
+							"Se a linha atual for menor que o último índice,
+							então eu posso adicionar mais +1 e mostrar na tela"
+						 *  
+						 */
 						if((i-1) >= 0)
 							System.out.println("Up: " + matriz[i-1][j]);
 						
